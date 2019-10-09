681fe529b57f2a457a0249bddbf174262c8d8533
dillon <dillon@dillons-mbp.3520.dhcp.asu.edu>
2019-10-09 12:19:46 -0700

    Initial version

---
+++ CSE360A2initialCode/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,66 @@
+package cse360assign2;
+/**
+ * @author Dillon Jayanthan
+ * Date: 10/9/19
+ * CSE360 Wednesday 70642
+ */
+
+public class AddingMachine
+{
+	/**
+	 * below variables are to keep a current record of the total and history of total's
+	 * modification.
+	 */
+	private int total;
+	
+	public AddingMachine ()
+	{
+		total = 0;
+		
+	}
+	
+	/**
+	 * the getTotal function returns the current value of total.
+	 * @return total
+	 */
+	public int getTotal ()
+	{
+		return 0;
+		
+	}
+	
+	/**
+	 * the add function adds the passed value to the total.
+	 * @param value
+	 */
+	public void add (int value)
+	{
+		
+	}
+	
+	/**
+	 * the subtract function subtracts the passed value from the total.
+	 * @param value
+	 */
+	public void subtract (int value)
+	{
+		
+	}
+	
+	/**
+	 * the toString function returns the current history of the total variable's modifications.
+	 */
+	public String toString ()
+	{
+		return "";
+		
+	}
+	
+	/**
+	 * the clear function clears the total and it's history and resets both back to 0.
+	 */
+	public void clear() 
+	{
+		
+	}
+}
